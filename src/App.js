f2cb8f2c97198d592aa7ccb5eb660554a4407267
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-06-14 03:41:21 -0400

    removed extra routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import StickyFooter from "./pages/StickyFooter";
-import Dashboard from "./pages/dashboard/Dashboard";
-import Album from "./pages/album/Album";
-import Blog from "./pages/blog/Blog";
-import Checkout from "./pages/checkout/Checkout";
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <Route exact path={["/home", "/"]} component={Home} />
-      <Route exact path={["/stickyfooter"]} component={StickyFooter} />
-      <Route exact path={["/dashboard"]} component={Dashboard} />
-      <Route exact path={["/album"]} component={Album} />
-      <Route exact path={["/blog"]} component={Blog} />
-      <Route exact path={["/checkout"]} component={Checkout} />
     </Router>
   );
 }
